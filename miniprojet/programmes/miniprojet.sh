953963ee56980c8171c66ae7158c02c62e2ec807
valentinefleith <valentine.fleith@gmail.com>
2023-10-25 11:50:51 +0200

    ajout correction arguments + essai de correction pour le code HTTP qui ne fonctionne pas

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -z $1 ]; then
+if [ $# -ne 1 ] || [ -f $1 ]; then
 	echo "Usage : ./miniprojet.sh CHEMIN"
 	exit
 fi
@@ -11,6 +11,11 @@ COUNT=1
 while read -r line;
 do
 	HTTP_CODE=$(curl -s -I ${line} | grep "HTTP" | egrep -o "[[:digit:]]{3}");
+	CORRECT_301=$(curl -s -I -L ${line} | grep "HTTP" | egrep -o "[[:digit:]]{3}");
+	IS_CORRECTED=$($CORRECT_301 | grep "200")
+	if [ $HTTP_CODE -eq "301" ] && [ $IS_CORRECTED = true ]; then
+		HTTP_CODE="200"
+	fi
 	echo -e "${COUNT}\t${HTTP_CODE}\t${line}";
 	((COUNT++));
 done < "$CHEMIN"
