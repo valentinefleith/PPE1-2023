9b603f33392a983af4fa8dc6507f06b3f9139018
valentinefleith <valentine.fleith@gmail.com>
2023-10-29 11:48:51 +0100

    ajout charset d'encodage de la page + correction petit bug lors de la modification d'URL

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -8,16 +8,18 @@ fi
 CHEMIN=$1
 COUNT=1
 
-while read -r line;
+while read -r URL;
 do
-	HTTP_CODE=$(curl -s -I ${line} | grep "HTTP" | egrep -o "[[:digit:]]{3}");
-	IS_CORRECTED=$(curl -s -I -L ${line} | grep "HTTP" | egrep -o "200");
+	HTTP_CODE=$(curl -s -I ${URL} | grep "HTTP" | egrep -o "[[:digit:]]{3}");
+	IS_CORRECTED=$(curl -s -I -L ${URL} | grep "HTTP" | egrep -o "200");
 	if [ $HTTP_CODE -eq "301" ] && [ $IS_CORRECTED = "200" ]; then
 		HTTP_CODE="200"
-		NEW_URL=$(curl -s -I -L ${line} | grep "location:" | sed "s/location: //g")
-		line=$NEW_URL
+		NEW_URL=$(curl -s -I -L ${URL} | grep "location:" | sed "s/location: //g" | sed "s/\r//g")
+		URL=$NEW_URL
 	fi
-	echo -e "${COUNT}\t${HTTP_CODE}\t${line}";
+
+	CHARSET=$(curl -s -I -o /dev/null -w "%{content_type}" ${URL} | egrep -o "charset=([[:alnum:]]|-)+" | sed "s/charset=//g")
+	echo -e "${COUNT}\t${HTTP_CODE}\t${URL}\t${CHARSET}";
 	((COUNT++));
 done < "$CHEMIN"
 
