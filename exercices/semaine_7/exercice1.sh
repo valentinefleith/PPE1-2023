22a58f12a6d1dd314e440a4267046269b4b260d6
valentinefleith <valentine.fleith@gmail.com>
2023-11-08 18:11:25 +0100

    version finale exercice

--- exercices/semaine_7/exercice1.sh
+++ exercices/semaine_7/exercice1.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 1 ] || [ ! -f $1 ]; then
+	echo "Usage : ./exercice1.sh TEXT.txt"
+	exit
+fi
+
 TEXT=$1
 
-egrep -o "\b[[:alpha:]]+\b" $TEXT 
+egrep -o "\b[[:alpha:]]+\b" $TEXT | tr "[[:upper:]]" "[[:lower:]]"
