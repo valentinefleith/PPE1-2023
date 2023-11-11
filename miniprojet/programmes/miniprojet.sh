63ab3c4e52aaffe4ec38f490173836a4f2d4dbb3
valentinefleith <valentine.fleith@gmail.com>
2023-11-11 22:53:36 +0100

    ajout transformation tableau html

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -14,11 +14,26 @@ then
 	exit
 fi
 
+OUTPUT_FILE="tableaux/table.html"
+echo "<html>
+<head>
+<meta charset='UTF-8'/>
+</head>
+<body>
+<table>
+<tr><th>Ligne</th><th>URL</th><th>HTTP</th><th>encoding</th>
+" > $OUTPUT_FILE
 lineno=1
 while read -r URL
 do
 	response=$(curl -s -I -L -w "%{http_code}" -o /dev/null $URL)
 	encoding=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
 	echo -e "$lineno\t$URL\t$response\t$encoding"
+	echo "<tr>
+	<td>$lineno</td><td>$URL</td><td>$HTTP</td><td>$response</td><td>$encoding</td>
+	</tr>
+	" >> $OUTPUT_FILE
 	lineno=$(expr $lineno + 1)
 done < "$URLS"
+
+echo "</table></body></html>" >> $OUTPUT_FILE
